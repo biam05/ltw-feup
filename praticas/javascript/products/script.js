223f05d5f38ae08dcdd206cb470ee5d5bd9b2c9c
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-11-20 09:57:28 +0000

    select and print

--- praticas/javascript/products/script.js
+++ praticas/javascript/products/script.js
@@ -1,8 +1,22 @@
-// Select table with id 'products'
-// Without 'defer'
-window.addEventListener('load', function() {
-    let products = document.getElementById('products')
-    console.log(products)
-})
-
-// With 'defer'
+// Select and print the form
+console.log("Form");
+let form = document.getElementsByTagName('form');
+console.log(form);
+// --- using outerHTML
+console.log("Form", form[0].outerHTML);
+ 
+// Select and print the quantity input
+console.log("Quantity Input");
+let quantity = document.querySelector("form input[name='quantity']");
+console.log(quantity);
+// --- using outerHTML
+console.log(quantity.outerHTML);
+ 
+// Select and print all inputs inside the form
+console.log("All Input");
+let input = document.querySelectorAll("form input");
+for(let i = 0; i < input.length; i++) {
+    console.log(input[i]); 
+    // --- using outerHTML
+    console.log(input[i].outerHTML);
+}
